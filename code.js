5b4570b2f6ca6db4c8604b3dbed3a1adf18da901
noahcanen <ncanen@uwyo.edu>
2025-02-12 17:16:40 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,10 @@
 function binarySearch(list, element) {
     var low = 0;
     var high = list.length - 1;
-    while (high >= low) {
+    while (high >= low)
+    {
         var mid = low + Math.floor((high - low) / 2);
-        if (list[mid]==element){return i;}
+        if (list[mid]==element){return mid;}
         if (list[mid] > element){high = mid - 1;}
         if (list[mid] < element){low = mid + 1;}
     }
